78fc6fd206fcf7911ae61e3d3800f1feda16cfb6
Shanosha <wootenm1@gmail.com>
2026-01-28 14:52:46 -0500

    added bulk discount function

---
+++ inventory-tracker/src/utils/bulkDiscount.ts
@@ -0,0 +1,15 @@
+import {Product} from "../modules/Product.js";
+import {PhysicalProduct} from "../modules/PhysicalProduct.js";
+
+// if product is a PhysicalProduct and weighs over 100, apply a 10 percent discont to the price
+// else return the original price 
+function bulkDiscountChecker(product: Product): Product {
+    let productWithDiscounts = product.price;
+    if((product instanceof PhysicalProduct) && (product.weight >= 100)){
+        productWithDiscounts = Number((product.price * .9).toFixed(2));
+    }
+    product.price = productWithDiscounts;
+    return product;
+}
+
+export {bulkDiscountChecker}
